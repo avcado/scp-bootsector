a87dbfb80fcbfe8b401f4faf2dbe1a54c5d49dff
avcado <devavcado@gmail.com>
2021-02-04 16:29:29 -0600

    add a configuration file

---
+++ src/config.asm
@@ -0,0 +1,18 @@
+; HOW TO USE
+; Set the settings
+; you want to change
+; to 'Y' if it is
+; enabled,
+
+; set it to 'N' 
+; if you want to
+; disable it.
+
+;-------
+; Settings
+; for starting
+; the game
+;-------
+
+isIntroEnabled:
+    db 'N', 0
