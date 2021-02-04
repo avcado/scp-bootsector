1f00cc164c8b449e6fa5491c5574150d568126d1
avcado <devavcado@gmail.com>
2021-02-04 08:24:08 -0600

    added stuff to scp.asm

--- src/scp.asm
+++ src/scp.asm
@@ -1,6 +1,19 @@
-jmp $
+[org 0x7c00]
+int 10h
+mov bx, bootMsg
+call printf
+
+xor ax, ax
+int 16h
+
+; TODO: Call the start game
+
+; jmp $
 
 %include "src/print.asm"
+
+bootMsg:
+    db 'Press any key to start.', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
